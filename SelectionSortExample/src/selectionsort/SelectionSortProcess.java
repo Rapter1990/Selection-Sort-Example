071602d7df8d72d6602d9bebaf653c738e63f004
Sercan Noyan Germiyanoğlu <sngermiyanoglu@hotmail.com>
2020-06-11 20:57:03 +0300

    Upload Project Files
    Upload Project Files

--- SelectionSortExample/src/selectionsort/SelectionSortProcess.java
+++ SelectionSortExample/src/selectionsort/SelectionSortProcess.java
@@ -82,20 +82,21 @@ public class SelectionSortProcess {
 
 		// Convert ArrayList to Array
 		Person[] persons = personList.toArray(new Person[personList.size()]);
-
+		
 		for (int i = 0; i < persons.length - 1; i++) {
-			int minIndex = i;
-			for (int j = i + 1; j < persons.length; j++) {
-				
-				if ((greaterThan(compTr, persons[j], persons[minIndex])) == false)
-					minIndex = j;				
-				if (i != minIndex) {
-					Person tmp = persons[i];
-					persons[i] = persons[minIndex];
-					persons[minIndex] = tmp;
-				}
-			}
-		}
+			Person min = persons[i];
+	        int minId = i;
+	        for (int j = i+1; j < persons.length; j++) {
+	            if ((greaterThan(compTr, persons[j], persons[minId])) == false) {
+	                min = persons[j];
+	                minId = j;
+	            }
+	        }
+	        // swapping
+	        Person tmp = persons[i];
+			persons[i] = persons[minId];
+			persons[minId] = tmp;
+	    }
 
 		// Convert Array to ArrayList
 		personList = new ArrayList<>(Arrays.asList(persons));
